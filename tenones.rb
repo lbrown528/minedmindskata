7e6da3f51215b87623bff94e6c6d43663602b2b9
lbrown528 <yunngod1200@gmail.com>
2017-01-13 13:51:59 -0500

    5 to chicken

--- tenones.rb
+++ tenones.rb
@@ -1,11 +1,21 @@
 #puts 1111111111
-10.times do |blueberrypie|
-	blueberrypie += 1
-	puts blueberrypie
+#10.times do |blueberrypie|
+	#blueberrypie += 1
+	#puts blueberrypie
+#end
+
+#1.upto(100) { |x| puts x }
+
+#for x in 1..100 do
+#	puts x
+#end
+
+100.times do |n|
+	n +=1
+	if n == 5 
+	puts "jerkchicken"
+else
+	puts n
+end
 end
 
-1.upto(100) { |x| puts x }
-
-for x in 1..100 do
-	puts x
-end
